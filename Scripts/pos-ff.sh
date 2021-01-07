a35c3ba2dee49d69d3c28324f4cac931d5fa3902
arzkar <arzkar.dev@gmail.com>
2021-01-07 20:31:45 +0530

    Added the scripts used to download and convert the epubs to txt using cron
    - pos-ff.sh
      - It downloads the epub using FanFicFare and then converts it to two txt files i.e. plain_text and mark_down
      - It requires the calibre-cli to convert the epub to txt
    - pos-regex.py
      - It removes the redundant characters like ###, *** etc from the txt files using regex
    - crontab
      - It is the crobtab config created to automate the whole process of downloading and converting the fanfic
    - timestamp.sh
      - It is used to add timestamp to the cron.log for better debugging

---
+++ Scripts/pos-ff.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Cron job started"
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate fanfic
+cd '~/Personal/Git/Quote Finder/POS Files/Downloaded'
+fanficfare -p https://www.fanfiction.net/s/11191235/1/Harry-Potter-and-the-Prince-of-Slytherin
+echo "Downloaded the fic from fanficfare"
+ebook-convert 'Harry Potter and the Prince of Slytherin-ffnet_11191235.epub' 'Harry Potter and the Prince of Slytherin_pt.txt' --txt-output-formatting plain 
+ebook-convert 'Harry Potter and the Prince of Slytherin-ffnet_11191235.epub' 'Harry Potter and the Prince of Slytherin_md.txt' --txt-output-formatting markdown
+echo "Conversion using calibre completed"
+cd ..
+python pos-regex.py
+echo "Cron job completed"
