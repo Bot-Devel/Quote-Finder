1a15877dae215865d2caf4d943253a1d8ca338d8
arzkar <arzkar.dev@gmail.com>
2021-01-20 01:11:01 +0530

    Added POS Dictionary feature and refactored the code
    - Divided utils.py into different modules and moved them to /utils
      - bot_status.py : It contains keep_alive() which ensures the bot is kept online
      - embed_pages.py : It contains embed_page(), index_page() & divide_chunks()
      - finder.py : It contains quote_find(), get_dict_index() & pos_dict()
      - search.py : It contains search_string() and search_dict()
      - string_processing.py : It contains chapter_processing() &  get_raw_string()
      - excelToJson_Dump.py : It contains ExcelToJson() which reads the xlsx file & then dumps to the json file
    - Added a new feature called POS Dictionary
      - Two commands called d and index were introduced
      - d is invoked to search the dictionary
      - index is invoked to show the dictionary index list
      - Renamed /Text Files to /data and added POS Dictionary xlsx & json
    - Updated requirements.txt and added pyproject.toml and poetry.lock
    - Updated main.py because the POS Dictionary feature was added

---
+++ utils/embed_pages.py
@@ -0,0 +1,58 @@
+from utils.string_processing import chapter_processing
+import discord
+from utils.finder import get_dict_index, quote_find
+
+
+def embed_page(arg, page=0):  # page=0 so that 1st page is sent first
+    """ Call quote_find() and process the chapter_title & chapter_url
+    and return the embed and page_limit
+    """
+    chapter_desription, chapter_heading, quote_found_ctr, page_limit = quote_find(
+        arg, page)
+    if quote_found_ctr == 1:  # to fix the  UnboundLocalError: local variable 'loc_of_and' referenced before assignment error
+        chapter_title, chapter_url = chapter_processing(
+            chapter_heading)
+    chapter_desription = chapter_desription + "\n" + \
+        "Page: "+str(page+1)+'/'+str(page_limit)
+    if quote_found_ctr == 1:
+        embed1 = discord.Embed(title=''.join(chapter_title),
+                               url=chapter_url,
+                               description=chapter_desription,
+                               colour=discord.Colour(0x272b28))
+        return embed1, page_limit
+    elif quote_found_ctr == 0:
+        embed1 = discord.Embed(
+            description="Quote not found!",
+            colour=discord.Colour(0x272b28))
+        return embed1, page_limit
+    elif quote_found_ctr == 2:
+        embed1 = discord.Embed(
+            description="No more quotes found!",
+            colour=discord.Colour(0x272b28))
+        return embed1, page_limit
+
+
+def index_page(i):
+    """ Call get_dict_index() & divide_chunks() and return
+    the embed & limit
+    """
+    des = get_dict_index()
+    # Divide the index list into chunks so that there are 10 in each page
+    res = list(divide_chunks(des, 10))
+    limit = len(res)
+    if i < limit:
+        embed1 = discord.Embed(title='POS Dictionary Index',
+                               description='\n'.join(res[i]),
+                               colour=discord.Colour(0x272b28))
+    else:
+        embed1 = discord.Embed(
+            description="No more index data!",
+            colour=discord.Colour(0x272b28))
+    return embed1, limit
+
+
+def divide_chunks(list1, n):
+    """ Divide the index list into 'n' equal chunks
+    """
+    for i in range(0, len(list1), n):
+        yield list1[i:i + n]
