e015894eaca590e7b7f0b95e4df1a177b7a362d1
arzkar <arzkar.dev@gmail.com>
2021-02-10 02:29:30 +0530

    Removed redundant info and made the code more readable

--- utils/embed_pages.py
+++ utils/embed_pages.py
@@ -7,39 +7,46 @@ def embed_page(arg, page=0):  # page=0 so that 1st page is sent first
     """ Call quote_find() and process the chapter_title & chapter_url
     and return the embed and page_limit
     """
+
     chapter_heading, chapter_desription, quote_found_ctr, page_limit = quote_find(
         arg, page)
+
     if quote_found_ctr == 1:  # to fix the  UnboundLocalError: local variable 'loc_of_and' referenced before assignment error
         chapter_title, chapter_url = chapter_processing(
             chapter_heading)
+
     chapter_desription = chapter_desription + "\n" + \
         "Page: "+str(page+1)+'/'+str(page_limit)
+
     if quote_found_ctr == 1:
         embed1 = discord.Embed(title=''.join(chapter_title),
                                url=chapter_url,
                                description=chapter_desription,
                                colour=discord.Colour(0x272b28))
-        return embed1, page_limit
+
     elif quote_found_ctr == 0:
         embed1 = discord.Embed(
             description="Quote not found!",
             colour=discord.Colour(0x272b28))
-        return embed1, page_limit
+
     elif quote_found_ctr == 2:
         embed1 = discord.Embed(
             description="No more quotes found!",
             colour=discord.Colour(0x272b28))
-        return embed1, page_limit
+
+    return embed1, page_limit
 
 
 def index_page(page=0):
     """ Call get_dict_index() & divide_chunks() and return
     the embed & limit
     """
+
     des = get_dict_index()
     # Divide the index list into chunks so that there are 10 in each page
     res = list(divide_chunks(des, 10))
     limit = len(res)
+
     if page < limit:
         embed1 = discord.Embed(title='POS Dictionary Index',
                                url="https://docs.google.com/spreadsheets/d/1k-GXwnmJGLtp_IUNCkPI-B4IT5u-qDBEEH7KwJPLBuA/edit?usp=sharing",
@@ -56,10 +63,12 @@ def dict_page(arg, page=0):
     title, description, quote_found_ctr, page_limit = pos_dict(arg, page)
     if quote_found_ctr == 1:
         chapter_url = "https://docs.google.com/spreadsheets/d/1k-GXwnmJGLtp_IUNCkPI-B4IT5u-qDBEEH7KwJPLBuA/edit?usp=sharing"
+
     elif quote_found_ctr == 0:
         title = ""
         chapter_url = ""
         description = "Dictionary data not found!"
+
     elif quote_found_ctr == 2:
         title = ""
         chapter_url = ""
@@ -69,11 +78,13 @@ def dict_page(arg, page=0):
                            url=chapter_url,
                            description=description,
                            colour=discord.Colour(0x272b28))
+
     return embed1, page_limit
 
 
 def divide_chunks(list1, n):
     """ Divide the index list into 'n' equal chunks
     """
+
     for i in range(0, len(list1), n):
         yield list1[i:i + n]
--- utils/excelToJson_Dump.py
+++ utils/excelToJson_Dump.py
@@ -7,46 +7,55 @@ def get_dict_index():
      to two index for each sheet and return the indices
     """
     file1 = "data/POS Dictionary.xlsx"
-    df1 = pd.read_excel(file1,
-                        "Sheet2")  # Read sheet2 first since that one contains all the word definitions
-    df2 = pd.read_excel(file1,
-                        "Sheet1")
+    # Read sheet2 first since that one contains all the word definitions
+    df1 = pd.read_excel(file1, "Sheet2")
+    df2 = pd.read_excel(file1, "Sheet1")
+
     df1.set_index('Title')
     df2.set_index('Title')
+
     df1.dropna()
     df2.dropna()
+
     index1 = df1['Title'].values[:]
     index2 = df2['Title'].values[:]
+
     index1 = index1[~pd.isnull(index1)]
     index2 = index2[~pd.isnull(index2)]
+
     return index1, index2
 
 
 def ExcelToJson():
     """ Reads the excel sheet and then writes to the json file
     """
+
     file1 = "data/POS Dictionary.xlsx"
     data = {}
     data['dictionary'] = []
     index1, index2 = get_dict_index()
+
     df1 = pd.read_excel(file1,
                         "Sheet2",
                         index_col=0)
     df2 = pd.read_excel(file1,
                         "Sheet1",
                         index_col=0)
+
     for i in range(len(index1)):
         data['dictionary'].append({
             # str(i+1) is used since initially i=0
             'title': str(i+1)+") "+str(index1[i]),
             'description': str(df1.loc[index1[i]][0]),
         })
+
     for j in range(len(index2)):
         i += 1  # In the prev loop, i=14 and we did i+1 during appending but we need it to be 15 for the next loop
         data['dictionary'].append({
             'title': str(i+1)+") "+str(index2[j]),
             'description': str(df2.loc[index2[j]][0]),
         })
+
     with open('data/POS Dictionary.json', 'w') as outfile:
         json.dump(data, outfile)
 
