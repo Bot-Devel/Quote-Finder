ae90ed530a7fda491b90389410111939f545a0c2
arzkar <arzkar.dev@gmail.com>
2021-03-02 23:14:34 +0530

    Updated POS Dictionary files and removed redundant code

---
+++ scripts/bl-regex.py
@@ -0,0 +1,50 @@
+import re
+
+pt_input = open(
+    "Downloaded/Black Luminary_pt.txt", "r")
+md_input = open(
+    "Downloaded/Black Luminary_md.txt", "r")
+pt_output = open(
+    "Processed/Black Luminary_pt.txt", "w")
+md_output = open(
+    "Processed/Black Luminary_md.txt", "w")
+
+
+pt_book = pt_input.read()
+md_book = md_input.read()
+
+# Test if the occurences of ### is equal to the number of chapters+1(The 1st line)
+test = re.findall(r'### ', md_book)
+print("Number of occurences of ### (Should be number of chapters+1)-bl: ", len(test))
+
+# Remove extra paragraphs and white spaces from both the two files
+# \n+ is used to capture the whitespaces
+pt_book_rws = re.sub(r'(?:\n+(\*+ *))?\n+', r'\n\n', pt_book, flags=re.M)
+md_book_rws = re.sub(r'(?:\n+(\*+ *))?\n+', r'\n\n', md_book, flags=re.M)
+
+# Remove *** from the files
+pt_book_rstar = re.sub(r'\n+ *\* *\* *\* *\n+', r'\n\n',
+                       pt_book_rws, flags=re.M)
+md_book_rstar = re.sub(r'\n+ *\* *\* *\* *\n+', r'\n\n',
+                       md_book_rws, flags=re.M)
+
+# Remove \*\*\* from the files
+pt_book_rstar1 = re.sub(r'\n+^\\\*\\\*\\\*$\n+', r'\n\n',
+                        pt_book_rstar, flags=re.M)
+md_book_rstar1 = re.sub(r'\n+^\\\*\\\*\\\*$\n+', r'\n\n',
+                        md_book_rstar, flags=re.M)
+
+# Remove \*\*\*\* from the files
+pt_book_rstar2 = re.sub(r'\n+^\\\*\\\*\\\*\\\*$\n+', r'\n\n',
+                        pt_book_rstar1, flags=re.M)
+md_book_rstar2 = re.sub(r'\n+^\\\*\\\*\\\*\\\*$\n+', r'\n\n',
+                        md_book_rstar1, flags=re.M)
+
+# Remove ### from the files
+pt_book_rstar3 = re.sub(r'### ', r'',
+                        pt_book_rstar2, flags=re.M)
+md_book_rstar3 = re.sub(r'### ', r'',
+                        md_book_rstar2, flags=re.M)
+
+pt_output.write(pt_book_rstar3)
+md_output.write(md_book_rstar3)
--- scripts/pos-ff.sh
+++ scripts/pos-ff.sh
@@ -3,12 +3,19 @@ echo "Cron job started"
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate fanfic
 
-cd '/home/arbaaz/Personal/Git/Quote Finder/POS Files/Downloaded'
+cd '~/Personal/Git/Quote Finder/Files/Downloaded'
 fanficfare -up https://www.fanfiction.net/s/11191235/1/Harry-Potter-and-the-Prince-of-Slytherin
+fanficfare -up https://www.fanfiction.net/s/12125300/1/Black-Luminary
+
 echo "Downloaded the fic from fanficfare"
 ebook-convert 'Harry Potter and the Prince of Slytherin-ffnet_11191235.epub' 'Harry Potter and the Prince of Slytherin_pt.txt' --txt-output-formatting plain 
 ebook-convert 'Harry Potter and the Prince of Slytherin-ffnet_11191235.epub' 'Harry Potter and the Prince of Slytherin_md.txt' --txt-output-formatting markdown
+
+ebook-convert 'Black Luminary-ffnet_12125300.epub' 'Black Luminary_pt.txt' --txt-output-formatting plain 
+ebook-convert 'Black Luminary-ffnet_12125300.epub' 'Black Luminary_md.txt' --txt-output-formatting markdown
+
 echo "Conversion using calibre completed"
 cd ..
 python pos-regex.py
+python bl-regex.py
 echo "Cron job completed"
