3d085190f4c2f97201e924aa12a87a000b0a5919
rawmeat898 <34800658+rawmeat898@users.noreply.github.com>
2021-06-08 21:42:50 +0100

    Adding ACI100's Harry Potter and the Ashes of Chaos fanfiction to the Bot
    - exts/cogs/help.py: Mass change to the layout of the help function. Removed the The Sinister Man's Harry Potter and the Prince of Slytherin only items.
    
    - exts/utils/channels.py: Added AoC white list and cool down channels by adding aoc_channel_cooldown and aoc_channel_whitelist and introducing a Book 3.
    
    - utils/chapter_processing.py:  Chapter heading for AoC; done in the same fashion as YakAge's Black Luminary under Book 3.
    - utils/processing.py: Calls Book 3 .txt file Harry Potter and the Ashes of Chaos_md.txt to be searched.

--- exts/utils/channels.py
+++ exts/utils/channels.py
@@ -5,12 +5,18 @@ pos_channel_cooldown = [x.strip() for x in (config.get(
 
 bl_channel_cooldown = [x.strip() for x in (config.get(
     'cooldown channels', 'bl_channel')).split(",")]
+    
+aoc_channel_cooldown = [x.strip() for x in (config.get(
+    'cooldown channels', 'aoc_channel')).split(",")]
 
 pos_channel_whitelist = [x.strip() for x in (config.get(
     'whitelist channels', 'pos_channel')).split(",")]
 
 bl_channel_whitelist = [x.strip() for x in (config.get(
     'whitelist channels', 'bl_channel')).split(",")]
+    
+aoc_channel_whitelist = [x.strip() for x in (config.get(
+    'whitelist channels', 'aoc_channel')).split(",")]
 
 
 def check_channel(channel_id, reset_flag):
@@ -24,12 +30,17 @@ def check_channel(channel_id, reset_flag):
 
         book = 2
         channel = bl_channel_cooldown+bl_channel_whitelist
+        
+    elif str(channel_id) in aoc_channel_cooldown+aoc_channel_whitelist:
+
+        book = 3
+        channel = aoc_channel_cooldown+aoc_channel_whitelist
 
     else:
         reset_flag = True
 
     # reset cooldown if whitelist channel
-    if str(channel_id) in pos_channel_whitelist+bl_channel_whitelist:
+    if str(channel_id) in pos_channel_whitelist+bl_channel_whitelist+aoc_channel_whitelist:
         reset_flag = True
 
     return book, channel, reset_flag
